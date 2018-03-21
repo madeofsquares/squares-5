3fdec8ef9c1f9b19b8de11c72edc9fe844442407
madeofsquares <steve@madeofsquares.co.uk>
2018-03-21 21:17:57 +0000

    Added style for pagination

--- template_parts/pagination.php
+++ template_parts/pagination.php
@@ -1,4 +1,5 @@
 <?php the_posts_pagination( array(
-    'prev_text'         =>  'Previous',
-    'next_text'         =>  'Next'
+    'prev_text'         => 'Previous',
+    'next_text'         => 'Next',
+    'mid_size'          => 1,
 ) ); ?>
